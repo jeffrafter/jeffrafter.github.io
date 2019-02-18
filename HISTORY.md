55386ef77d58495aaa70b41ec0f5a0ca803470bd
Jeff Rafter <jeffrafter@gmail.com>
2019-02-17 16:03:05 -0800

    Initial commit

---
+++ HISTORY.md
@@ -0,0 +1,17 @@
+Cloned from:
+
+repo: https://github.com/resir014/gatsby-starter-typescript-plus
+sha: e498216183aff8bcb79ec496b5846bef5e259430
+
+Detatched fork:
+
+```bash
+git clone https://github.com/resir014/gatsby-starter-typescript-plus jeffrafter.github.io
+cd jeffrafter.github.io/
+rm -rf .git
+git init
+yarn
+touch HISTORY.md
+git add .
+git commit -a "Initial commit"
+```
---
+++ src/layouts/index.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
+
+import 'modern-normalize'
+import '../styles/normalize'
+
+import Header from '../components/Header'
+import LayoutRoot from '../components/LayoutRoot'
+import LayoutMain from '../components/LayoutMain'
+
+type StaticQueryProps = {
+  site: {
+    siteMetadata: {
+      title: string
+      description: string
+    }
+  }
+}
+
+const IndexLayout: React.SFC = ({ children }) => (
+  <StaticQuery
+    query={graphql`
+      query IndexLayoutQuery {
+        site {
+          siteMetadata {
+            title
+            description
+          }
+        }
+      }
+    `}
+    render={(data: StaticQueryProps) => (
+      <LayoutRoot>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: data.site.siteMetadata.description },
+            { name: 'keywords', content: 'gatsbyjs, gatsby, javascript, sample, something' }
+          ]}
+        />
+        <Header title={data.site.siteMetadata.title} />
+        <LayoutMain>{children}</LayoutMain>
+      </LayoutRoot>
+    )}
+  />
+)
+
+export default IndexLayout
