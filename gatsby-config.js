9fe2b709fd0f338d0626474a3b7898b5d0778046
Jeff Rafter <jeffrafter@gmail.com>
2019-06-03 20:32:53 -0700

    Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-61511214-2`,
       },
     },
     `gatsby-plugin-feed`,
