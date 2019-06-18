7699f9e8eeae60063a9f8c6604b6c39c20ce3031
Jeff Rafter <jeffrafter@gmail.com>
2019-06-18 09:26:49 -0700

    Write a small about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -16,7 +16,15 @@ export default class Tags extends React.Component<Props> {
     return (
       <Layout title={siteTitle}>
         <Head title="All tags" keywords={[`blog`, `gatsby`, `javascript`, `react`]} />
-        <article>About Jeff Rafter...</article>
+        <article>
+          <p>👋 Hi.</p>
+          <p>
+            I'm Jeff. I'm a Senior Software Engineer <strong>@GitHub.</strong> where I get to invent
+            and work and play all at the same time. I have a great family (Ali, Jude &amp; Gavin)
+            and a great dog (Stella). I studied Creative Writing and English Literature at the
+            University of Iowa and once upon a time wrote some techinical books.
+          </p>
+        </article>
       </Layout>
     )
   }
